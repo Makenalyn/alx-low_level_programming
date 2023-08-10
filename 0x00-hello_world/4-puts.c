452b0297b3d4043f88fc46a7c3edeb8a2e1c9b68
Makenalyn <makenalynton@gmail.com>
2023-08-10 09:10:28 -0700

    a C program that prints exactly a statement as provided

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point takes parameter
+ * Return: 0 Indicates success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
