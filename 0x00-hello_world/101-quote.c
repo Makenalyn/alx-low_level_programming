94251fa1a747885e1374a5f629bf91cf7057adef
Makenalyn <makenalynton@gmail.com>
2023-08-10 10:12:27 -0700

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description:- prints the sizes of various characters
+ *
+ * Return:0 Always
+ */
+
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
