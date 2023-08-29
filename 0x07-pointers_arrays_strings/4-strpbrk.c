772726dafe0d6391494e4d095d5f1cd085a66024
Makenalyn <makenalynton@gmail.com>
2023-08-29 10:19:42 -0700

    function locates the first occurrence in the string s of any of the bytes in the string accept

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any set of bytes
+ * @s: string variable
+ * @accept: the set of bytes to search for
+ *
+ * Return: pointer to the first occurenceof bytes
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (&s[i]);
+}
