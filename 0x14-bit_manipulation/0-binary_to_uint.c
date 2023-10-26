37bf54be8a4674be98db46441056ddd311ea896e
Makenalyn <makenalynton@gmail.com>
2023-10-26 11:34:42 -0700

    changes a number from binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,6 +6,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int result = 0;
 	unsigned int i = 0;
 
+	if (b == NULL)
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '1' || b[i] == '0')
