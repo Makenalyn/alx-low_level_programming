36ff5883202d3b818c2aee30bdac0aa675c85b22
Makenalyn <lyntonmakena@gmail.com>
2023-11-27 12:04:31 -0800

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * create_file - creates a new file
+ * @filename: name of the file created
+ * @text_content: content of the file
+ *
+ * Return: file created
+ */
+int create_file(const char *filename,
+		__attribute__((unused))char *text_content)
+{
+	size_t op;
+
+	op = open(filename, O_RDWR | O_CREAT);
+
+	close(op);
+
+	return (op);
+}
