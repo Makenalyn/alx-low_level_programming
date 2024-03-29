427f04e14343a8e62a8feccb7ddfe8d82e9d9e4b
Makenalyn <makenalynton@gmail.com>
2024-03-29 03:49:01 -0700

    header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -27,7 +30,5 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#include <stddef.h>
-#include <stdio.h>
 
 #endif
