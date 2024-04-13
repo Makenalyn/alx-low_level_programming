68e99d28c3454af9a87494c9ae85a153cc1a6e5e
Makenalyn <makenalynton@gmail.com>
2024-04-13 13:22:44 -0700

    returns the index of the hash key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,4 +1,21 @@
 #include "hash_tables.h"
 
+unsigned long int hash_djb2(const unsigned char *str);
+
+/**
+ * key_index - generates the index of the key
+ * @key: the key of the hash table
+ * @size: size of the hash table
+ *
+ * Return: key index
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{}
+{
+	unsigned long int keyindex;
+	unsigned long int hashkey;
+
+	hashkey = hash_djb2(key);
+	keyindex = hashkey % size;
+
+	return (keyindex);
+}
