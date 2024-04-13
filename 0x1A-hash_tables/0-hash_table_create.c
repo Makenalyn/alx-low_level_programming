2ad3d688a519f4256f1ea650f75d7e1e932a493f
Makenalyn <makenalynton@gmail.com>
2024-04-13 12:58:24 -0700

    assigns the size of the table to table size pointer

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
 
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
+
+	table->size = size;
 
 	return (table);
 }
